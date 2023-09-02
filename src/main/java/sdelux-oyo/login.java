0f27cceaf89c373987e6c9914762495fc5a68214
delux <delux@gmail.com>
2023-09-02 07:01:38 +0000

    delux oyo dev project

---
+++ src/main/java/sdelux-oyo/login.java
@@ -0,0 +1,13 @@
+package sdelux-oyo;
+
+/**
+ * Hello world!
+ *
+ */
+public class login.java 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
